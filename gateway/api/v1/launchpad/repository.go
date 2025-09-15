d418ecdefd0fc78b5393d1caea4a41220cead282
NhaTruong <tt912002@gmail.com>
2025-09-15 15:46:58 +0700

    feat (gateway/api/v1/launchpad/repository.go): Add func Create

--- gateway/api/v1/launchpad/repository.go
+++ gateway/api/v1/launchpad/repository.go
@@ -1,1 +1,19 @@
 package launchpad
+
+import (
+	"gateway/models"
+
+	"gorm.io/gorm"
+)
+
+type LaunchpadRepository struct {
+	db *gorm.DB
+}
+
+func NewLaunchpadRepository(db *gorm.DB) *LaunchpadRepository {
+	return &LaunchpadRepository{db: db}
+}
+
+func (r *LaunchpadRepository) Create(lp *models.Launchpad) error {
+	return r.db.Create(lp).Error
+}
